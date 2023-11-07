64817042d80b74278fd21069e406616780b2c88c
Jmini <woaalsdl12@gmail.com>
2023-11-07 15:55:11 +0900

    account finish

--- FortunaProject/account/views.py
+++ FortunaProject/account/views.py
@@ -1,19 +1,34 @@
+import json
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
 from .forms import SignupForm
+import os
+
+config_path = os.path.join(settings.BASE_DIR, 'config.json')
+with open(config_path, 'r') as config_file:
+    config_data = json.load(config_file)
 
 def login_view(request):
     if request.method == 'POST':
         team_name = request.POST['name']
         password = request.POST['password']
-        team = authenticate(request, name=team_name, password=password)
+        team = authenticate(request, username=team_name, password=password)  # Change 'name' to 'username' if your user model uses 'username' field
         if team is not None:
             login(request, team)
-            return redirect('index')
-    return render(request, 'account/login.html')
+            return redirect('index')  # Ensure you have 'index' view or URL configured in your urls.py
+        else:
+            # It's good to give some feedback when login fails
+            context = {
+                'error': "Invalid team name or password.",
+                'config': config_data['login'],
+            }
+            return render(request, 'account/login.html', context)
+    else:
+        context = {
+            'config': config_data['login'],
+        }
+        return render(request, 'account/login.html', context)
 
 def signup_view(request):
     if request.method == 'POST':
