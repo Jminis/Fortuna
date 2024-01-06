8a9972f6f0b27fe7fb47bc84200f806ebf294f13
Jmini <woaalsdl12@gmail.com>
2024-01-06 19:40:41 +0900

    240106

--- FortunaProject/account/urls.py
+++ FortunaProject/account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('create/', views.create_team_view, name='create_team'),
+    
     path('logout/', views.logout_view, name='logout'),
     path('delete/<int:team_id>/', views.delete_team_view, name='delete_team'),
 
--- FortunaProject/adminpage/urls.py
+++ FortunaProject/adminpage/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from account import views as account
 
 urlpatterns = [
     path('', views.admin_home_view, name='admin_home'),
+    path('create/', account.create_team_view, name='create_team'),
     # 추가 URL 패턴들...
 ]
